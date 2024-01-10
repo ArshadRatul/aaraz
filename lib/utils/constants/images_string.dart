8a16258b6926661c3c22a23b89735b5ee41523f7
ArshadRatul <ratularshad@gmail.com>
2024-01-10 14:59:25 +0600

    added the logos and other needed classes

---
+++ lib/utils/constants/images_string.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Timages{
+  //app logos
+  static const String AppLogo = "assets/logos/arraz.png";
+
+  //social media logo
+  static const String facebook = "assets/logos/facebook.png";
+  static const String google = "assets/logos/google.png";
+
+}
