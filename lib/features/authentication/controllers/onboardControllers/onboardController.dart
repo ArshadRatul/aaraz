0adff09407838d63ac771adfdcd5b0652829ecfc
ArshadRatul <ratularshad@gmail.com>
2024-01-13 23:28:55 +0600

    added the on screen page

---
+++ lib/features/authentication/controllers/onboardControllers/onboardController.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+
+/// This is a controller class to control the onboard screens
+class OnBoardController extends GetxController{
+  ///variables
+
+  ///Update current page
+  void updatePageIndicator(index){}
+
+  ///Jump to the specific dot selected page.
+  void dotNavigationClick(index){}
+
+  /// Update current index and go to new page
+  void nextPage(){}
+
+  ///Update current page and go to last page
+  void skipPage(){}
+
+}
