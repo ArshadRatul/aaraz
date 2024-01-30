b6ae839f06e72595c26b6e9c888c50074a3c97b4
ArshadRatul <ratularshad@gmail.com>
2024-01-30 22:40:36 +0600

    Updated the onboarding screed adding the controllers

--- lib/features/authentication/screens/onboard.dart
+++ lib/features/authentication/screens/onboard.dart
@@ -1,4 +1,5 @@
 import 'package:aaraz/features/authentication/screens/widgets/OnboardBottomNavigation.dart';
+import 'package:aaraz/features/authentication/screens/widgets/OnboardNextButton.dart';
 import 'package:aaraz/features/authentication/screens/widgets/OnboardScreens.dart';
 import 'package:aaraz/utils/constants/colors.dart';
 import 'package:aaraz/utils/constants/images_string.dart';
@@ -6,19 +7,26 @@ import 'package:aaraz/utils/constants/size.dart';
 import 'package:aaraz/utils/constants/text.dart';
 import 'package:aaraz/utils/helpers/helper.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+import '../controllers/onboardControllers/onboardController.dart';
 
 class OnBoardScreen extends StatelessWidget {
   const OnBoardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(OnBoardController());
     return Scaffold(
       body: Stack(
         children: [
           // Top changing slide shows
           PageView(
+            controller: controller.pageController,
+            onPageChanged: controller.updatePageIndicator,
             children: const [
               onboardscreen(
                 image: Timages.onBoardScreen1,
@@ -39,21 +47,19 @@ class OnBoardScreen extends StatelessWidget {
           ),
           
           // Skip button
-          Positioned(top: THelperFunction.getAppBarHeight(),right: TSize.defaultSpacing, child: TextButton(onPressed: (){}, child: const Text("Skip"),)),
+          Positioned(
+              top: THelperFunction.getAppBarHeight(),
+              right: TSize.defaultSpacing,
+              child: TextButton(onPressed: (){},
+                child: const Text("Skip"),
+              )
+          ),
 
           //Dot navigation
           OnBoardingNavigation(),
 
           //button
-          Positioned(
-              right: TSize.defaultSpacing,
-              bottom: THelperFunction.getBottomNavigationBarHeight(),
-              child: ElevatedButton(
-                  onPressed: (){},
-                style: ElevatedButton.styleFrom(shape: CircleBorder()),
-                child: const Icon(Iconsax.arrow_right_3),
-              )
-          )
+          onboardNextButton()
 
 
         ],
@@ -61,6 +67,8 @@ class OnBoardScreen extends StatelessWidget {
     );
   }
 }
+
+
 
 
 
